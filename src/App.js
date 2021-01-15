47700f3fc08f209e01f9143df2e547343a2c8eb3
anjashardian <anjashardian@gmail.com>
2021-01-15 17:31:09 +0700

    remedial_anjas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Axios from 'axios'
+import { Route, Switch } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import component
+import Navbar from './component/navbar'
+
+// import page
+import Home from './page/home'
+import LoginPage from './page/login'
+import CartPage from './page/cart'
+import HistoryPage from './page/history'
+
+// import action
+import { LOG_IN, getHistory } from "./action"
+
+class App extends React.Component {
+  componentDidMount() {
+    Axios.get(`http://localhost:2000/users?id=${localStorage.getItem('id')}`)
+      .then(res => {
+        this.props.LOG_IN(res.data[0])
+
+        Axios.get(`http://localhost:2000/history?idUser=${localStorage.getItem('id')}`)
+          .then(res => {
+            this.props.getHistory(res.data)
+          })
+          .catch(err => console.log(err))
+      })
+      .catch(err => console.log(err))
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path='/' component={Home} exact />
+          <Route path='/login' component={LoginPage} />
+          <Route path='/cart' component={CartPage} />
+          <Route path='/history' component={HistoryPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(null, { LOG_IN, getHistory })(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+import App from "./App";
+
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import Reducers from "./reducer";
+
+let globalState = createStore(Reducers);
+globalState.subscribe(() =>
+  console.log("Global State : ", globalState.getState())
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <Provider store={globalState}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
